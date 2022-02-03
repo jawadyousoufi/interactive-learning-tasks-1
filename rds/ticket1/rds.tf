0f0742e64ddf5a779d12b1f1fb5a66f96a3540f0
jawadyousoufi <javadyousefi21@gmail.com>
2022-02-03 16:10:38 +0000

    rds ticket

--- rds/ticket1/rds.tf
+++ rds/ticket1/rds.tf
@@ -1,7 +1,15 @@
 resource "aws_db_instance" "default" {
-  # Your code goes here 
+  allocated_storage    = 50
+  engine               = "mysql"
+  engine_version       = "5.7"
+  instance_class       = "db.t3.micro"
+  name                 = "mydb"
+  username             = "foo"
+  password             = "foobarbaz"
+  parameter_group_name = "default.mysql5.7"
+  skip_final_snapshot  = true
   
-  
+  auto_minor_version_upgrade = true
   
   # Please do not change below code
   tags = local.task_tags
